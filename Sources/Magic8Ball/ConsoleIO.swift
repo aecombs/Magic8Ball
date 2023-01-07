4074c57e629f4094d31ef5818e77bc7a2deb4f98
Alicia Combs <alicia.combs.92@gmail.com>
2023-01-07 00:11:07 -0800

    In Magic8Ball, add all program's print statements and prompt for user interaction, exiting when prompted

--- Sources/Magic8Ball/ConsoleIO.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  
-//
-//  Created by Alicia Combs on 1/6/23.
-//
-
-import Foundation
--- Sources/Magic8Ball/Magic8Ball.swift
+++ Sources/Magic8Ball/Magic8Ball.swift
@@ -1,8 +1,7 @@
 @main
 public struct Magic8Ball {
     public static func main() {
-
-        // randomize and print Answer
+        // All the answers the Magic 8 Ball knows
         let answers: [String] = [
             "It is certain", "Reply hazy, try again",
             "Don’t count on it", "It is decidedly so",
@@ -15,6 +14,47 @@ public struct Magic8Ball {
             "Most likely", "Outlook good",
             "Yes", "Signs point to yes"]
         
-        print(answers.randomElement() ?? "Come back later")
+        // Introduction to user
+        print("\nHey there! I'm your friendly neighborhood Magic 8 Ball."
+              + "\nI've got answers to all of your questions."
+              + "\nWhy don't you take me for a spin?")
+        print("\nTo shake me up, you'll need to think of a question first."
+              + "\nGot one? Good!"
+              + "\n\nNow, hold it in your mind. Concentrate on it.")
+        
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 2) ??
+
+        
+        print("\n\nOkay. Ready? Let's do this!\n")
+        
+        // The loop in which we give answers and prompt for user input
+        var shouldQuit: Bool = false
+        while !shouldQuit {
+            print("\nShaking...")
+            print("\nShaking........")
+            print("\nShaking.................\n\n\n")
+            
+            print("Ah! Yes! I have your answer...\n\n")
+            print("~~~~~~~~")
+            print(answers.randomElement() ?? "\nCome back later")
+            print("~~~~~~~~")
+            
+            // Prompt user to continue or exit
+            print("\nWould you like to ask something else?\n('y' to go again, 'n' to quit)")
+            
+            if let input = readLine() {
+                switch input {
+                case "y", "yes":
+                    continue
+                case "n", "no", "q", "exit":
+                    shouldQuit = true
+                default:
+                    print("\nI'll take that as a yes.\n")
+                }
+            }
+        }
+        
+        // Exiting the program
+        print("\n\n\nI hope you found the answer you were looking for. Goodbye!")
     }
 }
